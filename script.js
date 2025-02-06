4d7c61ca0e1a8faefb3d0093908b77b5d8167d06
chillguy5 <tim.schiltmans@gmail.com>
2025-02-06 21:49:24 +0100

    .

--- script.js
+++ script.js
@@ -8,3 +8,15 @@ function jump(){
         character.classList.remove("animate");
     },500);
 }
+
+var checkDead = setinterval(function(){
+    var characterTop =
+    parseint(window.getComputedStyle(character).getPropertyvalue("top"));
+    var blockLeft =
+    parseint(window.getComputedStyle(block).getPropertyvalue("left"));
+    if (blockLeft<20 && blockLeft>0 &&characterTop>=130) {
+        block.style.animation = "none";
+        block.style.display = "none";
+        alert("U lose")
+    }
+},10)
