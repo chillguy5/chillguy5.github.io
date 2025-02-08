8b2cb7674d8f0fc0fd6865fb2abbe8f54e077c27
chillguy52 <gijs08@icloud.com>
2025-02-08 13:55:41 +0100

    Update gijs.js
    fixed js

--- gijs.js
+++ gijs.js
@@ -119,7 +119,14 @@ scene("battle", () => {
 		go("lose")
 	})
 
-	const boss = add([sprite(bossName), area(), scale(1), pos(width() / 2, 40), health(BOSS_HEALTH), anchor("top"), "boss", { dir: 1 }])
+	const boss = add([sprite(bossName), area(), scale(0.7), pos(width() / 2, 40), health(BOSS_HEALTH), anchor("top"), "boss", { dir: 1 }])
+
+	boss.onUpdate(() => {
+		boss.move(boss.dir * BOSS_SPEED, 0)
+		if (boss.pos.x < 0 || boss.pos.x > width()) {
+			boss.dir *= -1
+		}
+	})
 
 	const healthbar = add([rect(width(), 24), pos(0, 0), color(107, 201, 108), fixed(), { max: BOSS_HEALTH, set(hp) { this.width = width() * hp / this.max } }])
 
@@ -134,15 +141,6 @@ scene("battle", () => {
 
 	scene("win", () => {
 		add([text("YOU WIN!", { size: 48 }), pos(width() / 2, height() / 2), anchor("center")])
-		add([text("Press R to Restart", { size: 24 }), pos(width() / 2, height() / 2 + 40), anchor("center")])
-		add([text("Press M for Main Menu", { size: 24 }), pos(width() / 2, height() / 2 + 80), anchor("center")])
-
-		onKeyPress("r", () => go("battle"))
-		onKeyPress("m", () => window.location.href = "index.html")
-	})
-
-	scene("lose", () => {
-		add([text("YOU LOSE!", { size: 48 }), pos(width() / 2, height() / 2), anchor("center")])
 		add([text("Press R to Restart", { size: 24 }), pos(width() / 2, height() / 2 + 40), anchor("center")])
 		add([text("Press M for Main Menu", { size: 24 }), pos(width() / 2, height() / 2 + 80), anchor("center")])
 
