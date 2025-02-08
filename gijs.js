16822a0fe0f24a68432bddabb06e57941aa8e01b
chillguy52 <gijs08@icloud.com>
2025-02-09 00:47:15 +0100

    Update gijs.js
    test

--- gijs.js
+++ gijs.js
@@ -60,7 +60,7 @@ scene("battle", () => {
 		}
 	})
 
-	const player = add([sprite("player"), area(), scale(0.25), pos(width() / 2, height() - 100), anchor("center"), "player"])
+	const player = add([sprite("player"), area(), scale(0.40), pos(width() / 2, height() - 88), anchor("center"), "player"])
 
 	onKeyDown("left", () => {
 		player.move(-PLAYER_SPEED, 0)
@@ -82,10 +82,12 @@ scene("battle", () => {
 		music.speed = 1
 	})
 
-	function spawnBullet(p) {
-		add([rect(12, 48), area(), pos(p.sub(0, 20)), anchor("center"), color(127, 127, 255), outline(4), move(UP, BULLET_SPEED), offscreen({ destroy: true }), "bullet"])
-		play("shoot", { volume: 0.3, detune: rand(-1200, 1200) })
-	}
+	function spawnBullet() {
+    add([rect(12, 48), area(), pos(player.pos.x - 16, player.pos.y - player.height / 2), anchor("center"), color(127, 127, 255), outline(4), move(UP, BULLET_SPEED), offscreen({ destroy: true }), "bullet"])
+    add([rect(12, 48), area(), pos(player.pos.x + 16, player.pos.y - player.height / 2), anchor("center"), color(127, 127, 255), outline(4), move(UP, BULLET_SPEED), offscreen({ destroy: true }), "bullet"])
+    play("shoot", { volume: 0.3, detune: rand(-1200, 1200) })
+}
+
 
 	onKeyPress("space", () => {
 		spawnBullet(player.pos.sub(16, 0))
