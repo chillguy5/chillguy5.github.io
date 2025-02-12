7325add95cd880bbff6cf627e39ae7670760dda2
chillguy52 <gijs08@icloud.com>
2025-02-12 16:41:27 +0100

    Update gijs.js

--- gijs.js
+++ gijs.js
@@ -103,11 +103,9 @@ scene("battle", () => {
 	})
 
 function spawnBullet(p) {
-		add([
     add([rect(12, 48), area(), pos(p.sub(0, 20)), anchor("center"), color(127, 127, 255), outline(4), move(UP, BULLET_SPEED), offscreen({ destroy: true }), "bullet"])
     play("shoot", { volume: 0.3, detune: rand(-1200, 1200) })
-		])
-	}
+}
  
 	onUpdate("bullet", (b) => {
 		if (insaneMode) {
@@ -116,14 +114,9 @@ function spawnBullet(p) {
 	})
 
 
-
 	onKeyPress("space", () => {
 		spawnBullet(player.pos.sub(16, 0))
 		spawnBullet(player.pos.add(16, 0))
-		play("shoot", {
-			volume: 0.3,
-			detune: rand(-1200, 1200),
-		})
 	})
 
 	function spawnTrash() {
