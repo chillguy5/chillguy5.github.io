2d936265c5818723fe52df60deea2db565e1404c
chillguy52 <gijs08@icloud.com>
2025-02-13 13:50:22 +0100

    Update gijs.js

--- gijs.js
+++ gijs.js
@@ -141,8 +141,8 @@ function spawnBullet(p) {
 
 
 	onKeyPress("space", () => {
-		spawnBullet(player.pos.sub(60, 0))
-		spawnBullet(player.pos.add(60, 0))
+		spawnBullet(player.pos.sub(35, 10))
+		spawnBullet(player.pos.add(35, 10))
 	})
 
 	function spawnTrash() {
