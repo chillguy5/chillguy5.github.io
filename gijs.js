043e99a86f8792250b38d34c892cc8ab79c3e1b2
chillguy5 <tim.schiltmans@gmail.com>
2025-02-13 14:10:15 +0100

    ..

--- gijs.js
+++ gijs.js
@@ -236,18 +236,20 @@ healthbar.onUpdate(() => {
 
 			
 	onCollide("bullet", "boss", (b, e) => {
-		destroy(b)
-		play("hit")
-		e.hurt(1)
-		shake(1)
-		healthbar.set(e.hp())
-		e.hurt(insaneMode ? 10 : 1)
-		addExplode(b.pos, 1, 24, 1)
+		destroy(b);
+		play("hit");
+		e.hurt(1);
+		shake(1);
+		healthbar.set(e.hp());
+		e.hurt(insaneMode ? 10 : 1);
+		addExplode(b.pos, 1, 24, 1);
 		if (e.hp() <= 0) {
-			go("win")
-			play("explode")
+			coins += 100; // Voeg 100 coins toe
+			localStorage.setItem("coins", coins); // Sla de coins op in localStorage
+			go("win");
+			play("explode");
 		}
-	})
+	});
 
 	const timer = add([
 		text(0),
@@ -266,6 +268,7 @@ healthbar.onUpdate(() => {
 		add([text("YOU WIN!", { size: 48 }), pos(width() / 2, height() / 2), anchor("center")])
 		add([text("Press R to Restart", { size: 24 }), pos(width() / 2, height() / 2 + 40), anchor("center")])
 		add([text("Press M for Main Menu", { size: 24 }), pos(width() / 2, height() / 2 + 80), anchor("center")])
+		add([text(`Coins: ${coins}`, { size: 24 }), pos(width() / 2, height() / 2 + 120), anchor("center")]);
 
 		onKeyPress("r", () => go("battle"))
 		onKeyPress("m", () => window.location.href = "index.html")
