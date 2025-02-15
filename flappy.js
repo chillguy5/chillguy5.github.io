7541925dd1c286365647eb36fc9806d1dd858edc
chillguy5 <tim.schiltmans@gmail.com>
2025-02-15 17:18:10 +0100

    .

--- flappy.js
+++ flappy.js
@@ -64,10 +64,10 @@ scene("game", () => {
         { name: "samuelgame.png", scale: 0.25 },
         { name: "ardagame.png", scale: 0.3 },
 		{ name: "amirgame.png", scale: 0.45 },
-        { name: "chillguygame.png", scale: 0.15 },
+        { name: "chillguygame.png", scale: 0.1 },
         { name: "mangogame.webp", scale: 0.1 },
         { name: "johnporkgame.png",scale: 0.1 },
-        { name: "pessigame.png", scale: 0.15 }
+        { name: "pessigame.png", scale: 0.1 }
     ];
 
 	// Verkrijg de naam van het geselecteerde karakter uit localStorage
