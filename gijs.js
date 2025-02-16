444af62ffa11bf7c61c6a509dd6845a433039bf9
chillguy5 <tim.schiltmans@gmail.com>
2025-02-16 17:12:30 +0100

    .

--- gijs.js
+++ gijs.js
@@ -380,7 +380,6 @@ scene("win", () => {
     add([text(`Coins: ${coins}`, { size: 24 }), pos(width() / 2, height() / 2 + 120), anchor("center")]);
     add([text(`Score: ${score} seconds`, { size: 24 }), pos(width() / 2, height() / 2 + 160), anchor("center")]);
     add([text(`Highscore: ${highscores} seconds`, { size: 24 }), pos(width() / 2, height() / 2 + 200), anchor("center")]);
-
 	addButton("Restart", vec2(width() / 2, height() / 2 + 200), () => go("battle"));
 	addButton("Main Menu", vec2(width() / 2, height() / 2 + 300), () => {
 		window.location.href = "index.html";
@@ -393,7 +392,6 @@ scene("win", () => {
 		add([text("YOU LOSE!", { size: 48 }), pos(width() / 2, height() / 2), anchor("center")])
 		add([text(`Coins: ${coins}`, { size: 24 }), pos(width() / 2, height() / 2 + 120), anchor("center")]);
 		add([text(`Highscore: ${highscores} seconds`, { size: 24 }), pos(width() / 2, height() / 2 + 200), anchor("center")]);
-
 		addButton("Restart", vec2(width() / 2, height() / 2 + 200), () => go("battle"));
 		addButton("Main Menu", vec2(width() / 2, height() / 2 + 300), () => {
 			window.location.href = "index.html";
