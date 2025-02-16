b86f696268ffdc99b78c8ad858f67454e4e32636
chillguy5 <tim.schiltmans@gmail.com>
2025-02-16 16:56:16 +0100

    .

--- gijs.js
+++ gijs.js
@@ -387,21 +387,6 @@ scene("win", () => {
     onKeyPress("m", () => window.location.href = "index.html");
 });
 
-
-
-	scene("lose", () => {
-		add([text("YOU LOSE!", { size: 48 }), pos(width() / 2, height() / 2), anchor("center")])
-		add([text("Press R to Restart", { size: 24 }), pos(width() / 2, height() / 2 + 40), anchor("center")])
-		add([text("Press M for Main Menu", { size: 24 }), pos(width() / 2, height() / 2 + 80), anchor("center")])
-		add([text(`Highscore: ${highscores} seconds`, { size: 24 }), pos(width() / 2, height() / 2 + 120), anchor("center")]);
-
-		onKeyPress("r", () => go("battle"))
-		onKeyPress("m", () => window.location.href = "index.html")
-	})
-	
-	spawnTrash()
-})
-
 go("battle")
 
 scene("mainMenu", () => {
