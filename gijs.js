c111910fb260fceaa830df8a383be224b5ed9a24
chillguy5 <tim.schiltmans@gmail.com>
2025-02-16 16:59:36 +0100

    .

--- gijs.js
+++ gijs.js
@@ -394,11 +394,9 @@ scene("win", () => {
 	spawnTrash()
 })
 
-go("battle")
-
 scene("mainMenu", () => {
     add([text("Welcome to Chill Guy Jumper."), pos(width() / 2, height() / 4), anchor("center"), scale(2), color(248, 248, 215)]);
-    add([text("Highscore: " + highscore), pos(width() / 2, height() / 2 - 95), scale(2), anchor("center"), color(248, 248, 215)]);
+    add([text("Highscore: " + highscores), pos(width() / 2, height() / 2 - 95), scale(2), anchor("center"), color(248, 248, 215)]);
     addButton("Start Game", vec2(width() / 2, height() / 2), () => go("game"));
     addButton("Main Menu", vec2(width() / 2, height() / 2 + 100), () => {
         window.location.href = "index.html";
