311f0eeb4d1eff807f999b8848725a3b787bbdd3
chillguy5 <tim.schiltmans@gmail.com>
2025-02-19 00:09:15 +0100

    .

--- js/blackjack/main.js
+++ js/blackjack/main.js
@@ -139,8 +139,8 @@ $("#chip-50").click(function(){selectWager(50)});
 $("#chip-100").click(function(){selectWager(100)});
 $("#chip-200").click(function(){selectWager(200)});
 $("#chip-500").click(function(){selectWager(500)});
-$("#chip-1000").click(function(){selectWager(500)});
-$("#chip-2000").click(function(){selectWager(500)});
+$("#chip-1000").click(function(){selectWager(1000)});
+$("#chip-2000").click(function(){selectWager(2000)});
 $("#chip-allin").click(function(){selectWager("coins")});
 
 // Button activation
