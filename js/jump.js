8ca0b84275b9b0daf6ca0435d93368aac7783330
chillguy52 <gijs08@icloud.com>
2025-02-19 10:35:34 +0100

    fixed js

--- js/jump.js
+++ js/jump.js
@@ -206,4 +206,23 @@ scene("mainMenu", () => {
     });
 });
 
+//disables zoom
+$(document).ready(function(){
+    var keyCodes = [61, 107, 173, 109, 187, 189];
+   
+    $(document).keydown(function(event) {   
+      if (event.ctrlKey==true && (keyCodes.indexOf(event.which) != -1)) {
+        alert('disabling zooming'); 
+        event.preventDefault();
+       }
+    });
+   
+    $(window).bind('mousewheel DOMMouseScroll', function (event) {
+       if (event.ctrlKey == true) {
+         alert('disabling zooming'); 
+         event.preventDefault();
+       }
+     });
+   });
+
 go("mainMenu");
