bbb9bb09c11721a040483f8618d52eb6ae1c493e
chillguy5 <tim.schiltmans@gmail.com>
2025-02-21 14:04:23 +0100

    .

--- js/paarden.js
+++ js/paarden.js
@@ -162,24 +162,19 @@ localStorage.setItem('coins', coins); // Update in localStorage
 	var horse3 = new Horse('horse3', 20, 12);
 	var horse4 = new Horse('horse4', 20, 16);
 
-	if (amount === 0) {
-		Materialize.toast("You must select a bet to play", 1000);
-	} else if (coins < amount) {
-		Materialize.toast("You don't have so much chips to bet! Reset the game to continue" , 2000);
-	} else if (coins < amount)
-
 	//Event listener to the Start button
 	document.getElementById('start').onclick = function(){
 	amount = parseInt(document.getElementById('amount').value);
-	coins -= amount; // Verliezen
-	document.getElementById('coins').innerText = coins;
-	localStorage.setItem('coins', coins); // Update in localStorage
 
 	if (amount === 0) {
 		Materialize.toast("You must select a bet to play", 1000);
 	} else if (coins < amount) {
 		Materialize.toast("You don't have so much chips to bet! Reset the game to continue" , 2000);
 	}
+
+	coins -= amount; // Verliezen
+	document.getElementById('coins').innerText = coins;
+	localStorage.setItem('coins', coins); // Update in localStorage
 
 
 		num_lap = parseInt(document.getElementById('num_lap').value);
