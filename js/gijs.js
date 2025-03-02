2ed0c4d6d2e4baca3d1c71bef847b98d8957ac48
chillguy5 <tim.schiltmans@gmail.com>
2025-03-02 22:36:36 +0100

    .

--- js/gijs.js
+++ js/gijs.js
@@ -33,7 +33,7 @@ function addButton(txt, p, f) {
 }
 
 let coins = parseInt(localStorage.getItem("coins")) || 0;
-let highscores = localStorage.getItem("highscores") ? parseFloat(localStorage.getItem("highscores")) : Infinity;
+let highscores = localStorage.getItem("highscores123") ? parseFloat(localStorage.getItem("highscores123")) : Infinity;
 
 scene("start", () => {
     add([text("Jump Game"), pos(width() / 2, height() / 4), anchor("center"), scale(2)]);
